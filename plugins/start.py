3879a439ceeb415122246f26d65ca317b563a922
Pooja0033 <113719724+Pooja0033@users.noreply.github.com>
2022-12-09 07:16:11 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -71,8 +71,8 @@ async def start(client,message):
 	**Document Or Video** and enter new filename to rename it__
 	""",reply_to_message_id = message.id ,  
 	reply_markup=InlineKeyboardMarkup(
-	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ], 
-	[InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical") ]  ]))
+	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/Prv_35") ], 
+	[InlineKeyboardButton("Bot 🧐", url="https://t.me/Prv_35/6") ]  ]))
 	         
 
 
